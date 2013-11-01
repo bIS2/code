efe20f5ff66a4b75b106a7584f0534ad6164cfe3
Asley <asleyarbolez@gmail.com>
2013-10-31 22:48:39 -0400

    updating

--- app/views/holdingssets/index.blade.php
+++ app/views/holdingssets/index.blade.php
@@ -17,7 +17,7 @@
 			<th>
 			  <!-- <h3 style="width: 240px; display: inline-block;">Sys1</h3>
 			  <h3 style="display: inline-block;">f245a</h3> -->
-			  <input id="select-all" name="select-all" type="checkbox" value="1" /> Select all Holdingsetssss
+			  <input id="select-all" name="select-all" type="checkbox" value="1" /> Select all Holdingsets
 			</th>
 
 		</tr>
@@ -52,7 +52,22 @@
 							</thead>
 							<tbody>
 						<? $k = 0; $k++; ?>
-						
+							@foreach ($holdingsset -> holdings as $post)		
+								<tr>
+									<td><?php echo $post->f245b; ?></td>
+									<td><?php echo $post->f245c; ?></td>
+									<td><?php echo $post->ocrr_ptrn; ?></td>
+									<td><?php echo $post->f022a; ?></td>
+									<td><?php echo $post->f260a; ?></td>
+									<td><?php echo $post->f260b; ?></td>
+									<td><?php echo $post->f710a; ?></td>
+									<td><?php echo $post->f780t; ?></td>
+									<td><?php echo $post->f362a; ?></td>
+									<td><?php echo $post->f866a; ?></td>
+									<td><?php echo $post->f866z; ?></td>
+									<td><?php echo $post->f310a; ?></td>
+								</tr>
+							@endforeach
 							</tbody>
 						</table>
 					</div>
