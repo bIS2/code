d696a0358067c68fba057cf8b50352a440377bac
elpbatista <elpbatista@gmail.com>
2013-11-02 16:57:17 -0400

    working on better wiews

--- app/views/layouts/login.blade.php
+++ app/views/layouts/login.blade.php
@@ -2,27 +2,28 @@
 {{-- main --}}
 @section('main')
 		
-		<div class="jumbotron">
-      		<div class="container">
-        		<h1>bIS</h1>
-        		<big>Begleitendes Informationssystem</big>
-        		<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
-     		</div>
-    	</div>
-
+	<div class="jumbotron">
 		<div class="container">
-			<div class="row">
-				<div class="col-md-4 col-md-offset-4">
-					
-					<!-- Notifications -->
-					@include('notifications')
-					<!-- ./ notifications -->
+			<h1>bIS</h1>
+			<big>Begleitendes Informationssystem</big>
+			<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
+		</div>
+    </div>
 
-					<!-- Content -->
-					@yield('content')
-					<!-- ./ content -->
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4 col-md-offset-4">
+					
+				<!-- Notifications -->
+				@include('notifications')
+				<!-- ./ notifications -->
+
+				<!-- Content -->
+				@yield('content')
+				<!-- ./ content -->
 				
-				</div>
 			</div>
 		</div>
+	</div>
+
 @stop
