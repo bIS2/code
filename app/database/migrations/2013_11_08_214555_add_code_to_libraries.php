f99a04d84c0584257d53f5c2ee4eb89597e2177a
Damian Soto Platero <soto.platero@gmail.com>
2013-11-08 16:49:28 -0500

    agregando la migracion para el campo code en librerias

--- app/database/migrations/2013_11_08_214555_add_code_to_libraries.php
+++ app/database/migrations/2013_11_08_214555_add_code_to_libraries.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateLibrariesTable extends Migration {
+class AddCodeToLibraries extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,13 +12,10 @@ class CreateLibrariesTable extends Migration {
 	 */
 	public function up()
 	{
-		 Schema::create('libraries', function(Blueprint $table) {
-		 	$table->increments('id');
-		 	$table->string('name');
-		 	$table->timestamps();
+		Schema::table('libraries', function(Blueprint $table) {
+			$table->string('code')->nullable();
 		});
 	}
-
 
 	/**
 	 * Reverse the migrations.
@@ -27,7 +24,9 @@ class CreateLibrariesTable extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop('libraries');
+		Schema::table('libraries', function(Blueprint $table) {
+			$table->dropColumn('code');
+		});
 	}
 
 }
