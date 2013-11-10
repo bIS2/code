637d88d4ffc464be8cbffc126ddf7ce52cbdf702
Asley <asleyarbolez@gmail.com>
2013-11-10 16:50:21 -0500

    Updating

--- app/controllers/GroupsController.php
+++ app/controllers/GroupsController.php
@@ -48,7 +48,7 @@ class GroupsController extends BaseController {
 		if ($validation->passes()) {
 			$group->save();
 			$group->holdingssets()->attach(Input::get('holdingsset_id'));
-			return Redirect::route('holdingssets.index', ['group_id'=>$group->id]);
+			return Redirect::route('sets.index', ['group_id'=>$group->id]);
 		}
 
 		return Redirect::route('groups.create')
