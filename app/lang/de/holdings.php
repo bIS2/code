776937e9fc9cf7068c41fbd49b95615947c0e096
bIS2 <piguet@trialog.ch>
2013-11-20 11:31:39 -0500

    German language corrections

--- app/lang/de/holdings.php
+++ app/lang/de/holdings.php
@@ -3,17 +3,17 @@
 return array(
 
 	'title'   				=> 'Holdings',
-	'ok2' 					=> 'Bestätigt',
+	'ok2' 					=> 'Kontrolliert',
 	'comment' 				=> 'Kommentar',
 	'create_list'			=> 'Liste erstellen',
 	'lists'					=> 'Listen',
 	'move_to_list'			=> 'Verschiebe zu',
 	'tagged'				=> 'Markiert',
 	'annotated'					=> 'Annotiert',
-	'pending'				=> 'In Warteposition',
+	'pending'				=> 'Unkontrolliert',
 	'filter'				=> 'Filter',
 	'advanced_filter'		=> 'Weitere Filter',
-	'ungroup'					=> 'Gruppe auslösen',
+	'ungroup'					=> 'Gruppe auflösen',
 	'printer'				=> 'Drucken'
 
 );
