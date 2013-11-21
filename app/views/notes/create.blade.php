eae9d68a7881576b917232a4dc8495b50a0f8c7a
Damian Soto <soto.platero@gmail.com>
2013-11-21 12:26:08 -0500

    eliminar icono cerra ventana en la creacion de las notas

--- app/views/notes/create.blade.php
+++ app/views/notes/create.blade.php
@@ -3,7 +3,7 @@
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+          <!-- <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button> -->
           <h4 class="modal-title">{{{ trans('notes.title-create') }}}</h4>
         </div>
         	<form action="{{ route('notes.store') }}" method="post" data-remote="true" id='create-note'>
