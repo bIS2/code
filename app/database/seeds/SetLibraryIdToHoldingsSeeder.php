72c9222ad214a55510fba01f69670288ad0cb68c
Damian Soto Platero <soto.platero@gmail.com>
2013-12-10 19:44:38 -0500

    update holdings

--- app/database/seeds/SetLibraryIdToHoldingsSeeder.php
+++ app/database/seeds/SetLibraryIdToHoldingsSeeder.php
@@ -5,6 +5,7 @@ class SetLibraryIdToHoldingsSeeder extends Seeder {
     public function run()
     {
     	$holdings = Holding::all();
+    	print "hola";
 			foreach ( $holdings as $holding) {
 
 				$library = Library::where( 'sublibraries','like','%'.$holding->f852b.'%')->first();
