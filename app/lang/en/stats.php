7cd3fb3266cde2cc5cd599ff1fcbb0b0488f890f
bIS2 <piguet@trialog.ch>
2013-12-17 17:34:47 -0500

    Update

--- app/lang/en/stats.php
+++ app/lang/en/stats.php
@@ -2,19 +2,19 @@
 
 return array(
 
-	'total_holding'			=> 'All HOL total',
-	'total_ok'				=> 'All HOL ok',
-	'total_anottated'		=> 'All HOL annotated',
-	'total_delivery'		=> 'Own HOL delivered',
-	'holding_oks'			=> 'Own HOL ok',
-	'holding_annotated'		=> 'Own HOL annotated',
+	'total_holding'			=> 'All HOL',
+	'total_ok'				=> 'HOL ok',
+	'total_anottated'		=> 'HOL annotated',
+	'total_delivery'		=> 'HOL delivered',
+	'holding_oks'			=> 'HOL ok',
+	'holding_annotated'		=> 'HOL annotated',
 	'grouped' 				=> 'Grouped',
 	'ungrouped'  			=> 'Ungrouped',
 	'confirmed'  			=> 'Confirmed',
-	'confirmed_owner'  		=> 'Confirmed Owner',
-	'confirmed_aux'  		=> 'Confirmed Auxiliar',
-	'holdingsets_confirm'	=> '',
-	'holdingsets_confirm_details'	=> '',
-	'holding_oks_details'	=> '',
-	'holding_annotated_datails' => ''
+	'confirmed_owner'  		=> 'Confirmed Owner HOL',
+	'confirmed_aux'  		=> 'Confirmed Auxiliary HOL',
+	'holdingsets_confirm'	=> 'Last good HOS',
+	'holdingsets_confirm_details'	=> '(Details)',
+	'holding_oks_details'	=> '(Details)',
+	'holding_annotated_datails' => '(Details)'
 );
