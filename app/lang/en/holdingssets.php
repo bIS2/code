cb1875d090cd447af577cd0bf6393864970b836a
bIS2 <piguet@trialog.ch>
2013-12-16 22:09:56 -0500

    Correcting conflict in lang files

--- app/lang/en/holdingssets.php
+++ app/lang/en/holdingssets.php
@@ -4,7 +4,6 @@ return array(
 
 	'groups'   						=> 'Groups',
 	'title'   						=> 'Holdings Sets',
-<<<<<<< HEAD
 	'create_group' 					=> 'Create Group',
 	'title_create_group' 			=> 'Create a Group of Holdings Set',
 	'move_to_group'					=> 'Move to',
@@ -17,20 +16,6 @@ return array(
 	'orphan'						=> 'Orphan HOS',
 	'printer'						=> 'Print',
 	'all'							=> 'All',
-=======
-	'create_group' 				=> 'Create Group',
-	'title_create_group' 	=> 'Create a Group of Holdings Set',
-	'move_to_group'				=> 'Move to',
-	'ok' 									=> 'Correct',
-	'select_all_hos'			=> 'Select All Holdings Sets',
-	'add_holdings'				=> 'Add Holdings to HOS',
-	'oked'								=> 'Corrects',
-	'pending'							=> 'Pending',
-	'advanced_filter'			=> 'Advanced Filter',
-	'orphan'							=> 'Orphan HOS',
-	'printer'							=> 'Print',
-	'all'									=> 'All',
->>>>>>> 28744b320907c1d9709318f993c97e3dc4c1dc87
 	'click_to_view_in_other_tab' 	=> 'Click to view in other tab.',
 	'see_more_information' 			=> 'See more information',
 	'see_information_from_original_system' => 'See original information (external)',
@@ -39,24 +24,17 @@ return array(
 	'add_HOL_to_this_HOS'			=> 'Add HOL to this HOS',
 	'confirm_ok_HOS'				=> 'Confirm HOS to be ok',
 	'just_owner'					=> 'Owner',
-<<<<<<< HEAD
 	'just_aux'						=> 'Additional',
 	'only_owner_and_aux'			=> 'Both',
 	'clear_owner_filter'			=> 'Clear MAIN filter'
-);
-=======
-	'just_aux'						=> 'Aux',
-	'only_owner_and_aux'	=> 'Both',
-	'clear_owner_filter'	=> 'clear_owner_filter',
 	'drag_and_drop_into_a_grouptab_to_move_this_HOS_to_another_HosGroup'=> 'Drag and Drop into a Tab to move this HOS to a HOS Group',
 	'drag_and_drop_into_a_grouptab_to_add_this_HOS_to_a_HosGroup'				=> 'Drag and Drop into a Tab to add this HOS to a HOS Group',
-	'no_results_to_show'	=> 'There is no result to show',
-	'no_notes'	=> 'No notes for this Holding',
-	'ocurrence_patron'	=> 'Ocurrence patron',
-	'reserved_by'	=> 'Reserved by: ',
-	'on_behalf_of'	=> 'On behalf of: ',
-	'showing'	=> 'Showing ',
-	'of'	=> 'of ',
-	'order_hos_by' => 'Order HOS By',
+	'no_results_to_show'			=> 'There is no result to show',
+	'no_notes'						=> 'No notes for this Holding',
+	'ocurrence_patron'				=> 'Ocurrence patron',
+	'reserved_by'					=> 'Reserved by: ',
+	'on_behalf_of'					=> 'On behalf of: ',
+	'showing'						=> 'Showing ',
+	'of'							=> 'of ',
+	'order_hos_by' 					=> 'Order HOS By'
 );
->>>>>>> 28744b320907c1d9709318f993c97e3dc4c1dc87
