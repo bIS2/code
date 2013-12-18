ecf0a5bb2570bcb7107b5f8815c229b8169c39ca
Asley <asleyarbolez@gmail.com>
2013-12-17 21:49:06 -0500

    Updating

--- app/database/seeds/SetGroupAndHolNumberToHoldingssetsSeeder.php
+++ app/database/seeds/SetGroupAndHolNumberToHoldingssetsSeeder.php
@@ -10,11 +10,11 @@ class SetGroupAndHolNumberToHoldingssetsSeeder extends Seeder {
 				$groups_numbers = count(DB::select('select id from group_holdingsset where holdingsset_id = ?',[$id]));
 				// $holdingsset->holdings_numbers = $holdings_numbers; 
 				// $holdingsset->groups_numbers = $groups_numbers; 
-				print $id.':';
-				print $groups_numbers;
-				print '-';
-				print $holdings_numbers;
-				print ' ... ';
+				// print $id.':';
+				// print $groups_numbers;
+				// print '-';
+				// print $holdings_numbers;
+				// print ' ... ';
 				DB::table('holdingssets')
             ->where('id', $id)
             ->update(array('groups_number' => $groups_numbers, 'holdings_number' => $holdings_numbers));
