6ee952a45663325efd4172b747279172c79b407f
Asley <asleyarbolez@gmail.com>
2014-01-20 20:10:53 -0500

    Update

--- app/models/Holdingsset.php
+++ app/models/Holdingsset.php
@@ -89,7 +89,6 @@ class Holdingsset extends Eloquent {
     foreach ($owners as $owner) {
       $countauxs = count(Holding::whereHoldingssetId($owner)->whereIsAux('t')->lists('id'));
       $count_auxs_receiveds = ($countauxs > 0) ? count(Holding::whereHoldingssetId($this->id)->whereIsAux('t')->whereReceived('t')->lists('id')) : 0;
-      // if ($owner == 176) die($countauxs.'->'.$count_auxs_receiveds);
       if (!(in_array($owner, $result))) {
         if ($countauxs  == 0) $receiveds[] = $owner;
       }
@@ -97,10 +96,6 @@ class Holdingsset extends Eloquent {
         if ($count_auxs_receiveds == $countauxs['owner']) $receiveds[] = $owner;
       }
     }
-    // var_dump($owners);
-    // var_dump($auxs);
-    // var_dump($receiveds);
-    // die();
     $receiveds = (count($receiveds) > 0) ? $receiveds : [-1];
     return holdingsset::whereIn('id', $receiveds);
   }
