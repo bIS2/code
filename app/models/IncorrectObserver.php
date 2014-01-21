8c2f40a11e81f615d0635c4040b4940bfe2bab98
Damian Soto Platero <soto.platero@gmail.com>
2014-01-20 22:50:57 -0500

    update

--- app/models/IncorrectObserver.php
+++ app/models/IncorrectObserver.php
@@ -9,8 +9,11 @@ class IncorrectObserver {
        'object_type' => 'holdingsset',
        'object_id' => $model->holdingsset->id,
       ]);
+
+      // change to incorrected state
       foreach ($model->holdings()->lists('id') as $id) {
-	      State::create( [ 'holding_id' => $id, 'user_id' => Auth::user()->id, 'state'=>'incorrrected' ] )
+	      State::create( [ 'holding_id' => $id, 'user_id' => Auth::user()->id, 'state'=>'incorrrected' ] );
+
       }
 
     }
@@ -23,6 +26,7 @@ class IncorrectObserver {
        'object_id' => $model->holdingsset->id,
       ]);
 
+      // change to the previus state
       foreach ($model->holdings()->lists('id') as $id) {
       	$prev_state = Holding::find($id)->states()->last()->state;
 	      State::create( [ 'holding_id' => $id, 'user_id' => Auth::user()->id, 'state'=>$prev_state ] );
