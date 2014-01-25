a50bbf5d7ce3a22bb8bcfb7ca53d63d5ba0f4d32
bIS2 <piguet@trialog.ch>
2014-01-24 22:39:40 -0500

    Update

--- app/lang/de/lists.php
+++ app/lang/de/lists.php
@@ -2,9 +2,11 @@
 
 return array(
 
+	'name'				=> 'Bezeichnung der Liste',
 	'title'				=> 'Übersicht',
 	'type-control'   	=> 'Bestandskontrolle',
 	'type-delivery'		=> 'Ablieferung',
 	'type-unsolve'   	=> 'Nicht ablieferbar',
+	'worker'			=> 'Geht an',
 	
 );
