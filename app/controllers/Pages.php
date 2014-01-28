34d257d8c2cbfecc3f31770f2707589f4dfeff9d
Asley <asleyarbolez@gmail.com>
2014-01-27 22:41:34 -0500

    Updating

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -10,7 +10,9 @@ class Pages extends BaseController {
 	public function getIndex(){
 		if  (Input::has('lang')) return Redirect::to(Request::header('referer'));
 		$data['holdingsset_confirm'] 	= Confirm::take(10)->get();
-		$data['holdings_ok'] 					= Ok::take(10)->get();
+		$ids = Holding::inLibrary()->lists('holdings.id');
+		$ids[] = -1;
+		$data['holdings_ok'] 			= State::whereState('ok')->whereIn('holding_id', $ids);
 		$data['holdings_annotated'] 	= Note::take(10)->get();
 		$data['holdings_revised'] 		= Revised::take(10)->get();
 
--- public/assets/js/tools.js
+++ public/assets/js/tools.js
@@ -42,6 +42,10 @@ $(function(){
       $(this).addClass('label-default')
     }
   )
+  
+  $('a').on('click', function() {
+    $(this).tooltip('hide')
+  });
 
   $(document).on('keypress', function(event) {
     if (event.keyCode == 27) $('.modal .close').click();
