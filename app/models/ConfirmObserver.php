e115756374b30bdf1d5037f7b576fceab53a506f
Damian Soto Platero <soto.platero@gmail.com>
2014-03-10 20:50:11 -0500

    update

--- app/models/ConfirmObserver.php
+++ app/models/ConfirmObserver.php
@@ -16,10 +16,7 @@ class ConfirmObserver {
        'object_id' => $holdingsset_id,
       ]);
 
-      Holdingsset::find($holdingsset_id)
-      ->update([ 
-       'state' => 'ok'
-      ]);
+      Holdingsset::find($holdingsset_id)->update([ 'state' => 'ok' ]);
       $ids = $model->holdingsset->holdings()->lists('id');
       foreach ($ids as $id) {
 	      State::create( [ 'holding_id' => $id, 'user_id' => $user_id, 'state'=>'confirmed' ] );
