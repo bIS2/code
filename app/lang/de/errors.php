3ea720c2f5b21182e25639634093721e2c030e63
bIS2 <piguet@trialog.ch>
2014-03-13 13:41:49 -0400

    Update

--- app/lang/de/errors.php
+++ app/lang/de/errors.php
@@ -4,9 +4,8 @@ return array(
 
 	'attach_list_control'    	=> "In Liste sind nur Holdings möglich mit dem Status Bestätigt, Korrekt o Annotiert.",
 	'attach_list_delivery'    	=> "In der Liefer-Liste nur Holdings mit Status Revidiert und korrekten möglich.",
-	'list_in_blank'     		=> "Uch!!, Keine Liste ohne gültige Holding möglich.",
-	'attach_list_delivery'     	=> "In der Liefer-Liste nur Holdings mit Status Revidiert und korrekten möglich.",
-	'list_in_blank'     		=> "Uch!! Liste kann nicht erstellt werden, da Sie keine Holdings mit dem notwendigen Status ausgewählt haben.",
+	'list_in_blank'     		=> "Es wurden keine gültigen HOL für diesen Listentyp ausgewählt.",
+	'attach_list_delivery'     	=> "In der Liefer-Liste können nur HOL mit Status Revidiert und korrekten vorkommen.",
 	'attach_holding_in_list'	=> "Holding bereits in Liste",
-	'field_size_in_blank' 		=> "Please type size"
+	'field_size_in_blank' 		=> "Bitte Umfang angeben",
 );
