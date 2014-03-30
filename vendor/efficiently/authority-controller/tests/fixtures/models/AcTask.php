8e983683ef52092f7534c38d3b04a3f1f4d052ce
Damian Soto Platero <soto.platero@gmail.com>
2014-03-29 19:25:42 -0500

    .gitignore is now working

---
+++ vendor/efficiently/authority-controller/tests/fixtures/models/AcTask.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Eloquent\Model as Eloquent;
+
+class AcTask extends Eloquent
+{
+
+    public static $rules = [];
+
+    protected $fillable = ['name'];
+
+    public function acProject()
+    {
+        return $this->belongsTo('AcProject');
+    }
+
+}
