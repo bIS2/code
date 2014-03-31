04b677240d6355043ccb9f7471ef8f6944fab958
bIS2 <piguet@trialog.ch>
2014-03-31 14:40:06 -0400

    Update

--- app/views/pages/help.blade.php
+++ app/views/pages/help.blade.php
@@ -6,7 +6,7 @@
 
 <h3>{{ trans('general.handbook_title') }}</h3>
 <p>{{ trans('general.to_find_handbook1') }}<a href="{{ trans('general.to_find_handbook_link') }}">
-{{ trans('general.to_find_handbook_link') }}</a>{{ trans('general.to_find_handbook2') }}</p>
+{{ trans('general.to_find_handbook_link_text') }}</a>{{ trans('general.to_find_handbook2') }}</p>
 
 <h3>{{ trans('general.clear_cookies_title') }}</h3>
 <p>{{ trans('general.to_clear_cookies') }} <a href="/clearcookies">{{ trans('general.click_here') }}</a></p>
