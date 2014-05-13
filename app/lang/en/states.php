1e12e0dc9b13e6b2263278b6f77390bd755584f6
Damian Soto <soto.platero@gmail.com>
2014-05-13 20:04:18 +0200

    copiando en/states

---
+++ app/lang/en/states.php
@@ -0,0 +1,34 @@
+<?php 
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Site Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are used by the paginator library to build
+	| the simple pagination links. You are free to change them to anything
+	| you want to customize your views to better match your application.
+	|
+	*/
+
+	'annotated' 					=> 'Annotated by MAGUSER',
+	'blank' 						=> '(still without status)',
+	'burn' 							=> 'HOL eliminated physically',
+	'commented'						=> 'Commented by SPB',
+	'confirmed' 					=> 'HOS correct',
+	'delivery' 						=> 'Sent to SPB',
+	'integrated'					=> 'Completely integrated',
+	'no_result_states'				=> 'No actions until now',
+	'ok' 							=> 'HOL available',
+	'received'						=> 'Integrated in SPB',
+	'revised' 						=> 'Revised',
+	'revised_annotated' 			=> 'Annotated and back to BIBUSER',
+	'revised_ok'		 			=> 'HOL can be delivered to SPB',
+	'spare'							=> 'Spare HOL',
+	'title'							=> 'Actions executed',
+	'trash' 						=> 'To eliminate',
+	'pending' 						=> 'Pending',
+	'deleted' 						=> 'In SPB (end)',
+);
