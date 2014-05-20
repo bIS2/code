ec9de8b53a06cba51e5ea06d949c3adbbc654e77
Asley <asleyarbolez@gmail.com>
2014-05-20 17:44:57 -0400

    Up

--- app/models/ConfirmObserver.php
+++ app/models/ConfirmObserver.php
@@ -25,10 +25,10 @@ class ConfirmObserver {
       //Increment stat total confirmed Holdings Set
       Stat::first()->increment('sets_confirmed');
 
-      if ($model->holdings()->whereIsOwner('t')->exists())
+      if ($model->holdingsset->holdings()->whereIsOwner('t')->exists())
 	      Stat::first()->increment('sets_confirmed_owner');
 
-      if ($model->holdings()->whereIsAux('t')->exists())
+      if ($model->holdingsset->holdings()->whereIsAux('t')->exists())
 	      Stat::first()->increment('sets_confirmed_auxiliar');
 
     }
