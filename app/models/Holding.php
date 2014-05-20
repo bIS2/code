6e668affdf6753f09038fe4f8a24d1fbca89afc3
Asley <asleyarbolez@gmail.com>
2014-05-20 17:32:53 -0400

    Fixed T043. Deleted fill-data files.

--- app/models/Holding.php
+++ app/models/Holding.php
@@ -430,7 +430,7 @@ class Holding extends Eloquent {
     	$str = $this->clean($this->$field);
       $ret = (strlen($str) > $len) ? '<span class="pop-over" data-content="<strong>'.$str.'</strong>" data-placement="top" data-toggle="popover" data-html="true" type="button" data-trigger="hover">'.truncate($str, 30).'</span>' : $str;
     }
-
+    if ($field == 'state') { $ret = trans('states.'.$ret); }
     return $ret;
   }
 
