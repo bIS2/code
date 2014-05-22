4640ed82943b6368219d16cc148ace1b3070204e
Damian Soto Platero <soto.platero@gmail.com>
2014-05-22 13:38:14 -0500

    including fontawesone cdn in print layout

--- app/views/layouts/print.blade.php
+++ app/views/layouts/print.blade.php
@@ -19,7 +19,8 @@
 
 		<!-- CSS
 		================================================== -->
-        {{ Basset::show('public.css') }}
+    @stylesheets('public')
+		<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
 
 		<style>
 		@section('styles')
