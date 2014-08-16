e9cdfd820e4bbbbc754fc849b40f7ad1dd149b59
Asley <asleyarbolez@gmail.com>
2014-08-15 20:44:44 -0400

    Up

--- app/views/footer.blade.php
+++ app/views/footer.blade.php
@@ -1,5 +1,5 @@
 <!-- the footer includes credits and something else... -->
-<footer id="footer" class="affix-bottom" data-spy="affix" data-offset-bottom="0">
+<footer id="footer" class="affix-bottom" data-spy="affix" data-offset-bottom="0" style="background: transparent">
 	<div class="container">
 
 
