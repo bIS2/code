976aacc961d8164b1c2a748654567214588ef277
bIS2 <piguet@trialog.ch>
2014-12-18 11:18:22 +0100

    Update

--- app/lang/de/errors.php
+++ app/lang/de/errors.php
@@ -4,14 +4,14 @@ return array(
 
 	'attach_holding_in_list'	=> "Holding bereits in Liste",
 	'attach_list_control'    	=> "In Liste sind nur Holdings möglich mit dem Status Bestätigt, Korrekt o Annotiert.",
-	'attach_list_delivery'    	=> "In der Liefer-Liste nur Holdings mit Status Revidiert und korrekten möglich.",
-	'attach_list_elimination' => "In Elimination List only attach holding Commented or checked To eliminate",
+	'attach_list_delivery'    	=> "In der Liefer-Liste nur Holdings mit Status Revidiert oder Korrekt möglich.",
+	'attach_list_elimination' 	=> "In Löschliste nur können nur HOL mit Status Kommentiert oder Bestätigt (?) eingefügt werden.",
 	'field_size_in_blank' 		=> "Bitte Umfang angeben",
 	'list_in_blank'     		=> "Es wurden keine für diesen Listentyp gültigen HOL ausgewählt.",
-	'list_name_is_blank'		=> "Please, type the name List",
+	'list_name_is_blank'		=> "Bitte geben Sie der Liste einen Namen.",
 	'list_name_is_duplicate'	=> 'Namen schon vergeben.',
 	'field_note_in_blank'		=> 'Ein Feld muss etwas enthalten',
-	'notes_in_blank'			=> "Notes can't not blank",
+	'notes_in_blank'			=> "Notiz kann nicht leer bleiben.",
 	'select_notes_is_0'			=> 'Leeres Bemerkungsfeld',
 	'field_size_dispatchable_in_blank'	=> 'Bitte Abzugebender Bestand angeben',
 );
