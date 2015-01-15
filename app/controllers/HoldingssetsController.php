66d270743e571aea574b2a1cf49b82e62f66acaf
Asley <asleyarbolez@gmail.com>
2015-01-14 22:10:34 -0500

    Up

--- app/controllers/HoldingssetsController.php
+++ app/controllers/HoldingssetsController.php
@@ -419,9 +419,9 @@ class HoldingssetsController extends BaseController {
 		$HOSS = DB::select('select id,holdings_number from holdingssets WHERE holdings_number = 1 ORDER BY id ');//->get();
 
 		foreach ($HOSS as $HOS) {
-			var_dump($HOS->id);
-			var_dump($HOS->holdings_number);
-			// holdingsset_recall($HOS->id);
+			// var_dump($HOS->id);
+			// var_dump($HOS->holdings_number);
+			holdingsset_recall($HOS->id);
 		}
 		// $urltoredirect = '/sets/recallallhos/'.($init + 1);
 		// if ($HOSS) {
