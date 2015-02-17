be7731acf885bcd1879a1b6443c6365866cd859e
Asley <asleyarbolez@gmail.com>
2015-02-16 23:27:38 -0500

    Up

--- app/controllers/HoldingssetsController.php
+++ app/controllers/HoldingssetsController.php
@@ -546,18 +546,18 @@ class HoldingssetsController extends BaseController {
 		$counter = 1;
 		$idhol = $_GET['idhol'];
 		// while ($counter > 0) {
-			$holdings = Holding::whereHoldingssetId($idhol)->paginate(200);
-			// $counter = count($holdings);
-			foreach ($holdings as $holding) { 
-				$sys2 = $holding -> sys2;
-				$new866a = ($holding->f866aupdated == '') ? $holding->f866a : $holding->f866aupdated ;
-				var_dump($holding -> sys2);
-				var_dump($new866a);
-				// $new866a = '19 (1969)-';
-				// // $new866a .= ' ';
-				$newhol_nrm = normalize866a($new866a, $sys2);
-				Holding::find($holding->id)->update(['f866aupdated'=>$new866a, 'hol_nrm' => $newhol_nrm, 'recallholnrm' => 1]);
-			}
+			// $holdings = Holding::whereHoldingssetId($idhol)->paginate(200);
+			// // $counter = count($holdings);
+			// foreach ($holdings as $holding) { 
+			// 	$sys2 = $holding -> sys2;
+			// 	$new866a = ($holding->f866aupdated == '') ? $holding->f866a : $holding->f866aupdated ;
+			// 	var_dump($holding -> sys2);
+			// 	var_dump($new866a);
+			// 	// $new866a = '19 (1969)-';
+			// 	// // $new866a .= ' ';
+			// 	$newhol_nrm = normalize866a($new866a, $sys2);
+			// 	Holding::find($holding->id)->update(['f866aupdated'=>$new866a, 'hol_nrm' => $newhol_nrm, 'recallholnrm' => 1]);
+			// }
 			holdingsset_recall($idhol);
 		// }
 		return 'OK';
